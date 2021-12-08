5ce8b5cf76b940783f94e1174f3152ba51292ab7
Joan0018 <joanhau201018@hotmail.com>
2021-12-09 00:59:56 +0800

    Upload Whole System

---
+++ WAD_Assignment/Registration.aspx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WAD_Assignment
+{
+    public partial class Registration : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
